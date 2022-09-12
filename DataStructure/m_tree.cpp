443115522925bd35e9658c47612e54e8cded191a
HBY-STAR <21302010042@m.fudan.edu.cn>
2022-09-12 17:45:26 +0800

    add

--- DataStructure/m_tree.cpp
+++ DataStructure/m_tree.cpp
@@ -190,8 +190,96 @@ class AvlTree
 
     static const int ALLOWED_IMBALANCE = 1;
 
-    int height(AvlNode *t)const
+    int height(AvlNode *t) const
     {
-        return t==nullptr?-1:t->height;
+        return t == nullptr ? -1 : t->height;
+    }
+
+    void insert(const Comparable &x, AvlNode *&t)
+    {
+        if (t == nullptr)
+            t = new AvlNode{x, nullptr, nullptr};
+        else if (x < t->element)
+            insert(x, t->left);
+        else if (x > t->element)
+            insert(x, t->right);
+
+        balance(t);
+    }
+
+    void balance(AvlNode *&t)
+    {
+        if (t == nullptr)
+            return;
+        if (height(t->left) - height(t->right) > ALLOWED_IMBALANCE)
+        {
+            if (height(t->left->left) >= height(t->left->right))
+                rotateWithLeftChild(t);
+            else
+                doubleWithLeftChild(t);
+        }
+        else if (height(t->right) - height(t->left) > ALLOWED_IMBALANCE)
+        {
+            if (height(t->right->right) >= height(t->right->left))
+                rotateWithRightChild(t);
+            else
+                doubleWithRightChild(t);
+        }
+        t->height = max(height(t->left), height(t->right)) + 1;
+    }
+
+    void rotateWithLeftChild(AvlNode *&k2)
+    {
+        AvlNode *k1 = k2->left;
+        k2->left = k1->right;
+        k1->right = k2;
+        k2->height = max(height(k2->left), height(k2->right)) + 1;
+        k1->height = max(height(k1->left), k2->height) + 1;
+        k2 = k1;
+    }
+
+    void rotateWithRightChild(AvlNode *&k1)
+    {
+        AvlNode *k2 = k1->right;
+        k1->right = k2->left;
+        k2->left = k1;
+        k2->height = max(height(k2->left), height(k2->right)) + 1;
+        k1->height = max(height(k1->left), k2->height) + 1;
+        k1 = k2;
+    }
+
+    void doubleWithLeftChild(AvlNode *&k3)
+    {
+        rotateWithRightChild(k3->left);
+        rotateWithLeftChild(k3);
+    }
+
+    void doubleWithRightChild(AvlNode *&k3)
+    {
+        rotateWithLeftChild(k3->right);
+        rotateWithRightChild(k3);
+    }
+
+    void remove(const Comparable &x, AvlNode *&t)
+    {
+        if (t == nullptr)
+            return;
+        if (x < t->element)
+            remove(x, t->left);
+        else if (x > t->element)
+            remove(x, t->right);
+        else if (t->left != nullptr && t->right != nullptr)
+        {
+            t->element = findMin(t->right)->element;
+            remove(t->element, t->right);
+        }
+        else
+        {
+            BinaryNode *oldNode = t;
+            t = (t->left != nullptr) ? t->left : t->right;
+            delete oldNode;
+        }
+
+        balance(t);
     }
 };
