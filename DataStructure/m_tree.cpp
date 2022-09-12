eb18ecbe89de04ab3fd45c645efec31a7d438c19
HBY-STAR <21302010042@m.fudan.edu.cn>
2022-09-12 16:08:20 +0800

    add

--- DataStructure/m_tree.cpp
+++ DataStructure/m_tree.cpp
@@ -1,29 +1,30 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-template<typename Comparable>
+//二叉查找树
+template <typename Comparable>
 class BinarySearchTree
 {
 public:
     BinarySearchTree();
-    BinarySearchTree(const BinarySearchTree & rhs);
-    BinarySearchTree(BinarySearchTree && rhs);
+    BinarySearchTree(const BinarySearchTree &rhs);
+    BinarySearchTree(BinarySearchTree &&rhs);
     ~BinarySearchTree();
 
-    const Comparable & findMin() const;
-    const Comparable & findMax() const;
-    bool contains(const Comparable & x)const;
-    bool isEmpty()const;
-    void printTree(ostream & out = cout) const;
+    const Comparable &findMin() const;
+    const Comparable &findMax() const;
+    bool contains(const Comparable &x) const;
+    bool isEmpty() const;
+    void printTree(ostream &out = cout) const;
 
     void makeEmpty();
-    void insert(const Comparable & x);
-    void insert(Comparable && x);
-    void remove(const Comparable & x);
+    void insert(const Comparable &x);
+    void insert(Comparable &&x);
+    void remove(const Comparable &x);
 
-    BinarySearchTree & operator=(const BinarySearchTree&rhs);
-    BinarySearchTree & operator=(BinarySearchTree && rhs);
+    BinarySearchTree &operator=(const BinarySearchTree &rhs);
+    BinarySearchTree &operator=(BinarySearchTree &&rhs);
 
 private:
     struct BinaryNode
@@ -32,22 +33,109 @@ private:
         BinaryNode *left;
         BinaryNode *right;
 
-        BinaryNode(const Comparable&theElement,BinaryNode *lt,BinaryNode *rt)
-            : element{theElement},left{lt},right{rt}{}
+        BinaryNode(const Comparable &theElement, BinaryNode *lt, BinaryNode *rt)
+            : element{theElement}, left{lt}, right{rt} {}
 
-        BinaryNode( Comparable&&theElement,BinaryNode *lt,BinaryNode *rt)
-            : element{std::move(theElement)},left{lt},right{rt}{}
+        BinaryNode(Comparable &&theElement, BinaryNode *lt, BinaryNode *rt)
+            : element{std::move(theElement)}, left{lt}, right{rt} {}
     };
 
     BinaryNode *root;
 
-    void insert(const Comparable&x,BinaryNode*&t);
-    void insert(Comparable && x,BinaryNode*&t);
-    void remove(const Comparable &x,BinaryNode *&t);
-    BinaryNode*findMin(BinaryNode*&t)const;
-    BinaryNode*findMax(BinaryNode*&t)const;
-    bool contains(const Comparable&x,BinaryNode*t)const;
-    void makeEmpty(BinaryNode*&t);
-    void printTree(BinaryNode*t,ostream&out)const;
-    BinaryNode*clone(BinaryNode*t)const;
+    void insert(const Comparable &x, BinaryNode *&t);
+    void insert(Comparable &&x, BinaryNode *&t);
+    void remove(const Comparable &x, BinaryNode *&t);
+    BinaryNode *findMin(BinaryNode *&t) const;
+    BinaryNode *findMax(BinaryNode *&t) const;
+    bool contains(const Comparable &x, BinaryNode *t) const;
+    void makeEmpty(BinaryNode *&t);
+    void printTree(BinaryNode *t, ostream &out) const;
+    BinaryNode *clone(BinaryNode *t) const;
+
+public:
+    bool contains(const Comparable &x)
+    {
+        return contains(x, root);
+    }
+    bool contains(const Comparable &x, BinaryNode *t) const
+    {
+        if (t == nullptr)
+            return false;
+        else if (x < t->element)
+            return contains(x, t->left);
+        else if (x > t->element)
+            return contains(x, t->right);
+        else
+            return true;
+    }
+
+    void insert(const Comparable)
+    {
+        insert(x, root);
+    }
+    void insert(const Comparable &x, BinaryNode *&t)
+    {
+        if (t == nullptr)
+            t = new BinaryNode{x, nullptr, nullptr};
+        else if (x < t->element)
+            insert(x, t->left);
+        else if (x > t->element)
+            insert(x, t->right);
+        else
+            ;
+    }
+    void insert(Comparable &&x, BinaryNode *&t)
+    {
+        if (t == nullptr)
+            t = new BinaryNode{std::move(x), nullptr, nullptr};
+        else if (x < t->element)
+            insert(std::move(x), t->left);
+        else if (x > t->element)
+            insert(std::move(x), t->right);
+        else
+            ;
+    }
+
+    void remove(const Comparable &x)
+    {
+        remove(x, root);
+    }
+    void remove(const Comparable &x, BinaryNode *&t)
+    {
+        if (t == nullptr)
+            return;
+        if (x < t->element)
+            remove(x, t->left);
+        else if (x > t->element)
+            remove(x, t->right);
+        else if (t->left != nullptr && t->right != nullptr)
+        {
+            t->element = findMin(t->right)->element;
+            remove(t->element, t->right);
+        }
+        else
+        {
+            BinaryNode *oldNode = t;
+            t = (t->left != nullptr) ? t->left : t->right;
+            delete oldNode;
+        }
+    }
+
+    BinaryNode *findMin(BinaryNode *t) const
+    {
+        if (t == nullptr)
+            return nullptr;
+        if (t->left == nullptr)
+            return t;
+        return findMin(t->left);
+    }
+
+    BinaryNode *findMax(BinaryNode *t) const
+    {
+        if (t == nullptr)
+            return nullptr;
+        if (t->right == nullptr)
+            return t;
+        return findMax(t->right);
+    }
 };
