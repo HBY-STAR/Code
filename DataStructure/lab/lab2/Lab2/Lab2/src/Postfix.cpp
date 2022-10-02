f255bd766489374124762cfb288257f4c0a1876a
HBY-STAR <21302010042@m.fudan.edu.cn>
2022-10-02 17:33:55 +0800

    add

--- DataStructure/lab/lab2/Lab2/Lab2/src/Postfix.cpp
+++ DataStructure/lab/lab2/Lab2/Lab2/src/Postfix.cpp
@@ -10,19 +10,52 @@ using namespace std;
  * @brief Converts infix to postfix, only need '*' '/' '+' '-', does not need '(', ')'.
  * @return Postfix
  * */
-static string postfix(string infix) {
-    int i=0;
+static string postfix(string infix)
+{
+    int i = 0;
     string ret;
-    while(infix[i]!='\0')
+    Node<char> *temp;
+    MyStack<char> ops(100000);
+    while (infix[i] != '\0')
     {
-        if(infix[i]=='*'||infix[i]=='/')
+        if (infix[i] == '*' || infix[i] == '/')
         {
-            
+            if (ops.peek() == nullptr || ops.peek()->val == '+' || ops.peek()->val == '-')
+            {
+                temp = new Node<char>(infix[i]);
+                ops.push(temp);
+            }
+            else
+            {
+                temp = ops.pop();
+                ret += temp->val;
+                delete temp;
+            }
         }
-
-
+        else if (infix[i] == '+' || infix[i] == '-')
+        {
+            if (ops.peek() == nullptr)
+            {
+                temp = new Node<char>(infix[i]);
+                ops.push(temp);
+            }
+            else
+            {
+                while (ops.peek() != nullptr)
+                {
+                    temp = ops.pop();
+                    ret += temp->val;
+                    delete temp;
+                }
+                temp = new Node<char>(infix[i]);
+                ops.push(temp);
+            }
+        }
+        else
+        {
+            ret += infix[i];
+        }
         i++;
     }
-
-    return "";
+    return ret;
 }
