107b73adad646a6a56c6824dfe3dfa752a71584e
HBY-STAR <21302010042@m.fudan.edu.cn>
2022-10-09 22:51:01 +0800

    add matrix

--- DataStructure/lab/lab3/DS2022_LAB3/DS2022_LAB03_STU/src/SpecialMatrixOps.cpp
+++ DataStructure/lab/lab3/DS2022_LAB3/DS2022_LAB03_STU/src/SpecialMatrixOps.cpp
@@ -3,6 +3,24 @@
 
 using namespace std;
 
-vector<vector<int> > SpecialMatrixOps::recoverSymmetricMatrix(const vector<int> &compressedMatrix) {
-    //TODO
+vector<vector<int>> SpecialMatrixOps::recoverSymmetricMatrix(const vector<int> &compressedMatrix)
+{
+    int length = compressedMatrix.size();
+    vector<vector<int>> ret(length * (length + 1) / 2, vector<int>(length * (length + 1) / 2, 0));
+    for (int i = 0, j = 0, k = 0; i < length; i++)
+    {
+        if (j == k)
+        {
+            ret[j][k] = compressedMatrix[i];
+            j = 0;
+            k++;
+        }
+        else
+        {
+            ret[j][k] = compressedMatrix[i];
+            ret[k][j] = compressedMatrix[i];
+            j++;
+        }
+    }
+    return ret;
 }
