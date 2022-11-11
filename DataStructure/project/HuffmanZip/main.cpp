07de49618dc6d30038ff1ef964d2b4a9ca102a3c
HBY-STAR <21302010042@m.fudan.edu.cn>
2022-11-12 03:07:03 +0800

    add

--- DataStructure/project/HuffmanZip/main.cpp
+++ DataStructure/project/HuffmanZip/main.cpp
@@ -11,34 +11,24 @@ using namespace std;
 
 int main()
 {
-    PIMAGE img;
+    setcaption("HuffmanZip");
 
     initgraph(640, 480, 0);
 
-    //先随便画一些东西
-    setcolor(EGERGB(0xFF, 0xFF, 0x0));
-    setfillcolor(EGERGB(0xFF, 0x0, 0x80));
-    fillellipse(50, 50, 50, 50);
+    setfont(18, 0, "宋体");
+    // settextjustify(LEFT_TEXT, TOP_TEXT); //默认值
+    outtextxy(300, 0, "Hello Graphics");
 
-    img = newimage();
-    getimage(img, 0, 0, 160, 120);
+    settextjustify(RIGHT_TEXT, TOP_TEXT);
+    outtextxy(300, 50, "Hello Graphics");
 
-    int x, y;
-    //把img平铺在屏幕上，使用一个二重循环
-    for (y = 0; y < 8; ++y)
-    {
-        for (x = 0; x < 8; ++x)
-        {
-            //把img整个，拉伸画在指定的矩形里
-            //需要指定目标矩形，源矩形
-            //源矩形参数不能忽略，否则会和其它非拉伸功能的贴图函数混淆
-            putimage(x * 80, y * 60, 80, 60, img, 0, 0, 160, 120);
-        }
-    }
+    settextjustify(CENTER_TEXT, TOP_TEXT);
+    outtextxy(300, 100, "Hello Graphics");
+
+    settextjustify(LEFT_TEXT, BOTTOM_TEXT);
+    outtextxy(300, 50, "Hello Graphics");
 
     getch();
-
-    delimage(img);
 
     closegraph();
 
